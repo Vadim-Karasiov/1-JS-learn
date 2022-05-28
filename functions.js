7caa3d97e5bd861a0e58c06ffa7009c0151e1e2d
VadimKarasev <vadkarasev@gmail.com>
2022-05-28 14:50:30 +0300

    before second lesson

--- functions.js
+++ functions.js
@@ -11,5 +11,69 @@
 // }
 // console.log('[age]', age);
 
-var result = confirm('Do you wanna quit?');
-console.log('[result]', result);
+// var result = confirm('Do you wanna quit?');
+// console.log('[result]', result);
+
+//custom functions
+
+// function createString(msg, year) {
+
+//     if (year === 2022) return;
+    
+//     return msg + ', ' + year + '!!!!';
+// }
+
+// var message = 'Happy New Year' 
+
+// var string = createString(message, 2022);
+// console.log('[string]', string);
+
+
+// var firstName = 'John';
+
+// function sayHello() {
+//     console.log('Hello, my name is ' + firstName);
+    
+//     var firstName = 'Bob'; 
+// }
+
+// sayHello()
+// console.log('[firstName]', firstName);
+
+//-----------------------------
+// function add(a, b) {
+//     return a + b;
+// }
+// function multiply(a, b) {
+//     return a * b;
+// }
+
+// function getResultAndPrint(callback) {
+//     var result = callback(2, 3);
+
+//     console.log('[result]', result);
+// }
+
+// getResultAndPrint(add)
+// getResultAndPrint(multiply)
+
+//----------------------------------------
+
+// var divide = function abc(a, b) {
+//     return a / b;
+// };
+
+// var result = divide(10, 5);
+// console.log('[result]', result);
+
+
+//----------------------------- Recursion
+var saySomething = function abc() {
+    var a = 5;
+    a++;
+    console.log('Hello John');
+    if(a === 10) return;
+    abc();
+}
+
+saySomething();
