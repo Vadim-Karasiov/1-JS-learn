cabfc56dfd2a5623ed577855f6dd83c6baa9c3d4
VadimKarasev <vadkarasev@gmail.com>
2022-06-07 22:32:57 +0300

    after arrays lesson

---
+++ arrays.js
@@ -0,0 +1,252 @@
+'use strict'
+
+//создание массива
+// var skills = ['html', 'css', false, 23, { name: 'John'}];
+// console.log('[skills]', skills);
+
+//добавление в массив - !не использовать!
+var skills = ['html', 'css'];
+// skills[1] = 'js';
+// console.log('[skills]', skills);
+
+// for (var key in skills) { //не использовать в массивах!
+//     console.log('[key]', key);
+//     console.log('[skills[key]]', skills[key]);
+// }
+//равносильно выражению:
+// for (var i = 0; i < skills.length; i++) {
+    // console.log('[skills[i]]', skills[i]);
+// }
+
+//многомерный массив
+// var matrix = [  
+//     [1, 2, 3],
+//     ['a', 'b', 'c']
+// ];
+
+// console.log('[matrix[1][2]]', matrix[1][2]);
+
+
+
+//methods
+//push, pop
+skills.push('react', 'redux');
+
+skills.pop();
+
+// console.log('[skills]', skills);
+
+//shift, unshift
+skills.unshift('scss', 'webpack');
+skills.shift();
+
+// console.log('[skills]', skills);
+
+
+//modern methods-----------------------------
+//split
+var fullName = 'John Doe Williams';
+var elements = fullName.split(' ');
+// console.log('[elements]', elements); //['John', 'Doe', 'Williams']
+
+//join------------------------------------
+var aray = elements.join(" ");
+// console.log('[aray]', aray);  //John Doe Williams
+
+var students = [
+    {name: 'John'},
+    {name: 'Mike'},
+    {name: 'Bob'}
+];
+
+//indexOf, lastIndexOf----------------------
+var jsIndex = elements.indexOf('Williams');
+// console.log('[jsIndex]', jsIndex);  //2
+
+// var jsLastIndex = elements.lastIndexOf('Williams');
+// console.log('[jsLastIndex]', jsLastIndex);  //2
+
+var mikeIndex =  students.indexOf('Mike');
+// console.log('[mikeIndex]', mikeIndex);
+
+//slice----------------------------------------------
+var updatedStudents = students.slice(-2);
+// updatedStudents.push({name: 'George'});
+
+// console.log('[students]', students);
+// console.log('[updatedStudents]', updatedStudents);
+
+//splice--------------------------
+var bill = [
+    'margo',
+    '123',
+    'abc'
+];
+var indexToDelete = bill.indexOf('123');
+var deletedElements = bill.splice(indexToDelete, 2, 456, 'Aloha');
+// console.log('[bill]', bill);
+// console.log('[deletedElements]', deletedElements);
+
+//sort----------------------------------------------------------
+var chars = ['z', 'a', 'w', 'o', 'f'];	
+var nums = [12, 1, 121, 21, 35, -2];
+
+chars.sort();
+nums.sort();
+
+// console.log('[chars]', chars);
+// console.log('[nums]', nums);
+
+//reverse---------------------------------------------------------
+chars.reverse()
+// console.log('[chars]', chars);
+
+//concat
+var concattedArray = chars.concat('y', ['m', 'n']);
+// console.log('[concattedArray]', concattedArray);
+
+
+//sort------------------------------------------------------------
+// function compareAsNumeric (x, y) {
+//     return x - y;
+// }
+
+nums.sort(function (x, y) {
+    return x - y;
+});
+// console.log('[nums]', nums);
+
+var users = [
+    {
+        name: 'John',
+        age: 23
+    },
+    {
+        name: 'Mike',
+        age: 11
+    },
+    {
+        name: 'Bob',
+        age: 80
+    }
+];
+
+users.sort(function (user1, user2){
+    return user1.age - user2.age;
+});
+
+// console.log('[users]', users);
+
+
+//forEach------------------------------------------------
+users.slice(-2).forEach(function(myUser, idx, arr) {
+    // console.log('[users]', users);
+    // console.log('[arr]', arr);
+});
+
+// for (var i = 0; i <users.length; i++) {
+//     var user = users[i];
+//     console.log('[user.age]', user.age);
+// }
+
+
+//map-------------------------------------------------
+var transformedUsers = users.map(function(user) {
+    return '<li>' + user.name + '</li>';
+});
+
+// console.log('[transformedUsers]', transformedUsers);
+
+
+//filter-------------------------------------
+var nums = [1, 2, 3, 4, 5];
+
+var filteredNumbers = nums.filter(function(number){
+    return number < 3;
+});
+
+var filteredUsers = users.filter(function(user) {
+    return user.name !== 'Mike';
+});
+// console.log('[filteredUsers]', filteredUsers);
+// console.log('[filteredNumbers]', filteredNumbers);
+
+
+//some, every----------------------------------
+var allAdultUsers = users.every(function(user){
+    return user.age >= 18;
+});
+
+var age80 = users.some(function(user){
+    return user.age === 80;
+});
+
+// console.log('[allAdultUsers]', allAdultUsers);
+// console.log('[age80]', age80);
+
+
+//find findIndex-------------------------------
+var mike = users.find(function(user) {
+    return user.name === 'Mike'
+});
+// console.log('[mike]', mike);
+
+var mikeIndex = users.findIndex(function(user) {
+    return user.name === 'Mike'
+});
+users.splice(mikeIndex, 1);
+
+
+// console.log('[mikeIndex]', mikeIndex);
+// console.log('[users]', users);
+
+
+//reduce, reduceRight
+var numbers = [10, 20, 30, 40];
+
+var totalSum = numbers.reduce(function(result, number, idx, arr){
+    result += number; //0+10; 10+20...30+30...
+
+    return result;
+}, 0)
+
+// console.log('[totalSum]', totalSum);
+
+//Пример с чеками
+var bills = [
+    {
+        name: 'ATB',
+        price: 300
+    },
+    {
+        name: 'ATB',
+        price: 800
+    },
+    {
+        name: 'Varus',
+        price: 1500
+    },
+    {
+        name: 'Silpo',
+        price: 900
+    }
+];
+var initialValue = {
+    shops: [],
+    totalSum: 0,
+    maxPrice: 0
+};
+
+var sumOfBills = bills.reduce(function(result, shop){
+    if (!result.shops.includes(shop.name)){
+        result.shops.push(shop.name);
+    }
+    result.totalSum += shop.price;
+
+    if (result.maxPrice < shop.price){
+        result.maxPrice = shop.price;
+    }
+
+    return result;
+}, initialValue);
+console.log('[sumOfBills]', sumOfBills);
--- objects.js
+++ objects.js
@@ -58,41 +58,41 @@ var user2 = {
 // console.log('[mike]', mike);
 
 //функция клонирования
-function cloneObject(obj) {
-    var clone = {}; //создается чистый объект
+// function cloneObject(obj) {
+//     var clone = {}; //создается чистый объект
 
-    for (var key in obj) { //чтение ключей входящего объекта
-        var value = obj[key];
+//     for (var key in obj) { //чтение ключей входящего объекта
+//         var value = obj[key];
 
-        //value = {html: true, css: true, js: false}
-        if (typeof value === 'object') {
-            //т.к. skills на данном этапе не создан, надо создать
-            clone[key] = {};
+//         //value = {html: true, css: true, js: false}
+//         if (typeof value === 'object') {
+//             //т.к. skills на данном этапе не создан, надо создать
+//             clone[key] = {};
 
-            //key2 = html, css, js
-            for (var key2 in value) { 
-                //value2 = true, true, false
-                var value2 = value[key2];
-                //clone.skills.html = true...
-                clone[key][key2] = value2;
-            }
-        } else { //помещаем основной код клонирования для того чтобы не перезаписало объект
-            clone[key] = value; //клонирование их в новый
-        }
+//             //key2 = html, css, js
+//             for (var key2 in value) { 
+//                 //value2 = true, true, false
+//                 var value2 = value[key2];
+//                 //clone.skills.html = true...
+//                 clone[key][key2] = value2;
+//             }
+//         } else { //помещаем основной код клонирования для того чтобы не перезаписало объект
+//             clone[key] = value; //клонирование их в новый
+//         }
 
-    }
-    return clone; //возврат клонированного объекта
-}
+//     }
+//     return clone; //возврат клонированного объекта
+// }
 
-var mike = cloneObject(user);
-mike.name = 'Mike';
-mike.skills.js = true; //вложенный объект все равно останется ссылкой
+// var mike = cloneObject(user);
+// mike.name = 'Mike';
+// mike.skills.js = true; //вложенный объект все равно останется ссылкой
 
-console.log('[user]', user);
-console.log('[mike]', mike);
+// console.log('[user]', user);
+// console.log('[mike]', mike);
 
 
-//strings--------------------------------------------------
+//strings-методы строк------------------------------------------
 var text = 'Hello World 2022';
 
 // console.log('[text.length]', text.length);
@@ -106,20 +106,55 @@ var text = 'Hello World 2022';
 
 
 //substring - отрезает часть слова, ждет индекс первого и последнего символа
-console.log('[text.substring(1, 7)]', text.substring(1, 7));
+// console.log('[text.substring(1, 7)]', text.substring(1, 7));
 
 
 //substr - отрезает часть слова, ждет индекс первого и количество символов после него
-console.log('[text.substr (0, 5)]', text.substr (0, 5));
+// console.log('[text.substr (0, 5)]', text.substr (0, 5));
 
 
 //slice - отрезает часть слова, ждет индекс первого и последнего символа. Но в отличие
 //от substring понимает конечное значение с минусом, что позволяет указывать от конца.
-console.log('[text.slice(1, -1)]', text.slice(1, -1));
+// console.log('[text.slice(1, -1)]', text.slice(1, -1));
 
 //indexOf - находит индекс слова
-var index = text.indexOf('World');
-console.log('[index]', index);
+// var index = text.indexOf('World');
+// console.log('[index]', index);
 
 //можно сочетать с отрезанием слов.
-console.log('[text.substr(index, 5)]', text.slice(index, index+5));
+// console.log('[text.substr(index, 5)]', text.slice(index, index+5));
+
+
+//numbers-методы чисел------------------------------
+
+//Отрезание чисел после запятой - .toFixed()
+// var num = 10 /3;
+// var fixedNum = num.toFixed(2);
+// console.log('[fixedNum]', fixedNum);
+
+//Извлечение чисел из строки - parseInt()
+// var size = '255.35px';
+// var value = parseInt(size);
+// console.log('[value]', value);
+
+// var value = parseFloat(size);
+// console.log('[value]', value);
+
+//isNaN, Infinity, -Infinity------------------------
+// isNaN('abc' / 23);
+// Number.isNaN(NaN);
+
+// isFinite();
+// Number.isFinite();
+
+
+//Math---------------------------------------------
+Math.floor(1.5); //1 - округление вниз
+Math.ceil(1.5) //2 - округление вверх
+Math.round(1.5) //= 2 - округление по математическим правилам к целому числу
+Math.sqrt(9) //= 3 – квадратный корень
+Math.pow(2, 3) //= 8 – возведение в степень = [2 ** 3](плохая поддержка)
+Math.abs(-23) //= 23 – модуль	
+Math.random() // псевдослучайное значение, от 0 до 1. *50 - от 0 до 50
+Math.max(1, 2, 3, 4, 5) = 5 //– нахождение наибольшего числа
+Math.min(1, 2, 3, 4, 5) = 1 //– нахождение наименьшего числа
